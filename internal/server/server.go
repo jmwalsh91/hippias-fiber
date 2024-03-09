24e3adc758941fbd0ab3c1180fa51892415d250f
jmwalsh91 <jordan@jmwalsh.dev>
2024-03-08 22:52:46 -0500

    register routes,db connection, translate handlers to fiber

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,17 +1,34 @@
 package server
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/supabase-community/supabase-go"
 )
 
-type FiberServer struct {
+type Server struct {
 	*fiber.App
+	sb *supabase.Client
 }
 
-func New() *FiberServer {
-	server := &FiberServer{
-		App: fiber.New(),
+func New() *Server {
+	API_KEY := os.Getenv("API_KEY")
+	API_URL := os.Getenv("API_URL")
+	client, err := supabase.NewClient(API_URL, API_KEY, nil)
+	if err != nil {
+		panic(err)
 	}
+	server := &Server{
+		App: fiber.New(),
+		sb:  client,
+	}
+
+	server.App.Get("/book/id", server.getBook)
+	server.App.Get("/list", server.listBooks)
+	server.App.Get("/authors", server.listAuthors)
+	server.App.Get("/authors/:id", server.getAuthor)
+	server.App.Get("/books/author/:id", server.getBooksByAuthorID)
 
 	return server
 }

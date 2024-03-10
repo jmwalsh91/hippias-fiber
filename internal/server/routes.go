a34fc072168e8293fbe0df1363a8feb87f45f901
jmwalsh91 <jordan@jmwalsh.dev>
2024-03-09 23:59:20 -0500

    get, list, create course

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -186,3 +186,73 @@ func (s *Server) listBooks(c *fiber.Ctx) error {
 
 	return c.JSON(books)
 }
+
+/*
+*
+
+	 ____
+	/ ___|___  _   _ _ __ ___  ___  ___
+    | |   / _ \| | | | '__/ __|/ _ \/ __|
+	| |__| (_) | |_| | |  \__ \  __/\__ \
+	 \____\___/ \__,_|_|  |___/\___||___/
+	 *
+*/
+
+func (s *Server) listCourses(c *fiber.Ctx) error {
+	data, _, err := s.sb.From("courses").Select("*", "exact", false).Execute()
+	if err != nil {
+		log.Printf("Error querying courses: %v", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	var courses []Course
+	if err := json.Unmarshal(data, &courses); err != nil {
+		log.Printf("Error unmarshaling courses: %v", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	log.Printf("Courses: %+v", courses)
+	return c.JSON(courses)
+}
+
+func (s *Server) getCourse(c *fiber.Ctx) error {
+	courseID := c.Params("id")
+
+	data, _, err := s.sb.From("courses").Select("*", "exact", false).Eq("id", courseID).Execute()
+	if err != nil {
+		log.Printf("Error querying course: %v", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	var course Course
+	if err := json.Unmarshal(data, &course); err != nil {
+		log.Printf("Error unmarshaling course: %v", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	log.Printf("Course: %+v", course)
+	return c.JSON(course)
+}
+
+func (s *Server) createCourse(c *fiber.Ctx) error {
+	var course Course
+	if err := c.BodyParser(&course); err != nil {
+		log.Printf("Error parsing course: %v", err)
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	data, err := json.Marshal(course)
+	if err != nil {
+		log.Printf("Error marshaling course: %v", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	_, err = s.sb.From("courses").Insert(string(data)).Execute()
+	if err != nil {
+		log.Printf("Error inserting course: %v", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	log.Printf("Created course: %+v", course)
+	return c.JSON(course)
+}
